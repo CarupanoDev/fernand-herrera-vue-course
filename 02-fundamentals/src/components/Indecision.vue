8d1a428d9d5e98f89d15033fa4aaedfbaa2b1c5a
Santiago Medina <carupanodev@gmail.com>
2021-09-24 08:18:15 -0400

    watch() lesson

---
+++ 02-fundamentals/src/components/Indecision.vue
@@ -0,0 +1,81 @@
+<template>
+  <img src="https://via.placeholder.com/250" alt="bg">
+  <div class="bg-dark"></div>
+
+  <div class="indecision-container">
+
+    <input type="text" placeholder="Make a question" v-model="question">
+    <p>Remember to finish with an interrogation symbol (?)</p>
+
+    <div>
+      <h2>{{  question }}</h2>
+      <h1>Yes, No .... Thinking</h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Indecision",
+  data() {
+    return {
+      question: null
+    }
+  },
+  watch: {
+    question(value, oldValue) {
+      if( !value.endsWith('?') ) return
+
+      // TODO: Make HTTP request
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+img, .bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
+
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
+
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
+
+h1, h2 {
+  color: white;
+}
+
+h2 {
+  margin-top: 150px;
+}
+
+</style>
